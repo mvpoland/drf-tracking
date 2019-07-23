fa4f49726d3b590a84b637507c9f20afa10180f1
Lukasz Lesko <lukaszlesko@gmail.com>
2019-07-23 10:53:07 +0200

    .

--- rest_framework_tracking/mixins.py
+++ rest_framework_tracking/mixins.py
@@ -19,3 +19,27 @@ class LoggingErrorsMixin(LoggingMixin):
 
     def should_log(self, request, response):
         return response.status_code >= 400
+
+
+class LoggingWithUserAgentMixin(BaseLoggingMixin):
+    def initial(self, request, *args, **kwargs):
+        super(LoggingWithUserAgentMixin, self).initial(request, *args, **kwargs)
+        self.log['headers'] = self._clean_data(request.META)
+        self.log['client'] = self._detect_client(request)
+
+    def handle_log(self):
+        """
+        Hook to define what happens with the log.
+
+        Defaults on saving the data on the db.
+        """
+        print self.log
+        #APIRequestLog(**self.log).save()
+
+    def _detect_client(self, request):
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+        if 'VikingApp' in user_agent and 'iOS' in user_agent:
+            return 'VikingApp_iOS'
+        elif 'VikingApp' in user_agent:
+            return 'VikingApp_Android'
+        return 'Other_WebPossibly'
